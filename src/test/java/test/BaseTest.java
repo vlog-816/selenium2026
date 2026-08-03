023e986101b6c3bfd3b7bbfcf5a643fb45068de1
vlog-816 <vyvy.tester@gmail.com>
2026-08-03 17:28:28 +0700

    allure

--- src/test/java/test/BaseTest.java
+++ src/test/java/test/BaseTest.java
@@ -1,9 +1,18 @@
 package test;
 
+import io.qameta.allure.Allure;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
+
+import java.io.ByteArrayInputStream;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class BaseTest {
 
@@ -17,6 +26,28 @@ public class BaseTest {
     @AfterTest(alwaysRun = true)
     public void closeBrowser() {
         if (driver != null) driver.quit();
+    }
+
+    @AfterMethod
+    public void captureScreenshotOnFailure(ITestResult result) {
+        if (result.getStatus() == ITestResult.FAILURE) {
+
+            // 1. Lấy tên của test case vừa chạy
+            String methodName = result.getMethod().getMethodName();
+
+            // 2. Lấy thời gian hiện tại và format theo chuẩn bạn muốn
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH-mm-ss");
+            String time = LocalDateTime.now().format(formatter);
+
+            // 3. Ghép tên hàm và thời gian lại với nhau
+            String imageName = methodName + "_" + time;
+
+            System.out.println("Test failed! Taking screenshot: " + imageName);
+
+            // 4. Chụp ảnh và đính kèm vào Allure với tên vừa tạo
+            byte[] screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
+            Allure.addAttachment(imageName, new ByteArrayInputStream(screenshot));
+        }
     }
 
 }
